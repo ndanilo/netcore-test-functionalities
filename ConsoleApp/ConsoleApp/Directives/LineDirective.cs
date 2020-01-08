67f230fcc060411e6cee3ad58120aef2b3d916ca
madedanilonovais <danilo.novais@madeinweb.com.br>
2020-01-08 13:05:39 -0300

    line directive test done

---
+++ ConsoleApp/ConsoleApp/Directives/LineDirective.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Directives
+{
+    public class LineDirective
+    {
+        public void WriteLines()
+        {
+            Console.WriteLine("Normal line #1."); // Set break point here.
+#line hidden
+            Console.WriteLine("Hidden line.");
+#line default
+            Console.WriteLine("Normal line #2.");
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ConsoleApp.Directives;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestLineDirectives();
+
+            Console.WriteLine();
+            Console.WriteLine("ended!!!");
+        }
+
+        static void TestLineDirectives()
+        {
+            var lineDirective = new LineDirective();
+            lineDirective.WriteLines();
+        }
+    }
+}
